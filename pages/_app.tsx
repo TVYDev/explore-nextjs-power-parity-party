767e6114a28a4966e12cac25f66db84b16632c65
Vannyou Tang <vannyoutang23@gmail.com>
2024-06-16 14:53:49 +0700

    add ads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,27 @@ import type { AppProps } from "next/app";
 import type { LayoutProps } from "@vercel/examples-ui/layout";
 import { getLayout } from "@vercel/examples-ui";
 import "@vercel/examples-ui/globals.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component);
 
   return (
-    <Layout
-      title="Power parity pricing"
-      path="edge-middleware/power-parity-pricing"
-    >
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
+    <>
+      <Script
+        async
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5058491449778369"
+        crossOrigin="anonymous"
+      ></Script>
+      <Layout
+        title="Power parity pricing"
+        path="edge-middleware/power-parity-pricing"
+      >
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </>
   );
 }
